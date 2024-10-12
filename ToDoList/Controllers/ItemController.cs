78cfa71f36b8dc6733dee9df36a85cd70e2c98b9
abdoelsayedDragon1 <abdo.com.a777@gmail.com>
2024-10-12 23:19:53 +0300

    Add project files.

---
+++ ToDoList/Controllers/ItemController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public IActionResult Index(ToDoItem toDoItem)
+        {
+           var toDoitem = dbContext.ToDoItems.ToList();
+            return View(model: toDoitem);
+        }
+        [HttpGet]
+    public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(1) 
+                };
+
+                if (Request.Cookies.ContainsKey("username"))
+                {
+                    Response.Cookies.Delete("username");
+                }
+
+                Response.Cookies.Append("username", name, cookieOptions);
+                return RedirectToAction("Index");
+            }
+
+        return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(ToDoItem model)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.ToDoItems.Add(model);  
+                dbContext.SaveChanges();      
+
+                TempData["SuccessMessage"] = "Item created successfully!";
+                return RedirectToAction(nameof(Index));  
+            }
+
+            return View(model);  
+        }
+
+        public IActionResult Edit(int ToDoItemId)
+        {
+            var ToDoItem = dbContext.ToDoItems.Find(ToDoItemId);
+            if (ToDoItem != null)
+                return View(model: ToDoItem);
+            return RedirectToAction("NotFound", "Home");
+
+        }
+        [HttpPost]
+        public IActionResult Edit(ToDoItem toDoItem)
+        {
+            
+            dbContext.ToDoItems.Update(toDoItem);
+            dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int ToDoItemId)
+        {
+            dbContext.ToDoItems.Remove(new() { Id = ToDoItemId });
+            dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+       public DbSet<ToDoItem> ToDoItems { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+        var Connection = builder.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(Connection);
+    }
+    }
+}
